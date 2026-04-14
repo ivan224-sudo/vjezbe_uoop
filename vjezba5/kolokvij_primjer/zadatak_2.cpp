433866b3e3bf6a9fb1f24b1f8a205e73967fe21b
ivan224-sudo <ivannovak@unin.hr>
2026-04-14 22:27:24 +0200

    Update zadatak_2.cpp

--- vjezba5/kolokvij_primjer/zadatak_2.cpp
+++ vjezba5/kolokvij_primjer/zadatak_2.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+int main() {
+    string datum;
+
+    cout << "Unesite datum rodjenja (DD/MM/GGGG): ";
+    cin >> datum;
+
+    // Zamjena '/' s '.'
+    for (int i = 0; i < datum.length(); i++) {
+        if (datum[i] == '/') {
+            datum[i] = '.';
+        }
+    }
+
+    cout << "Datum rodjenja: " << datum << endl;
+
+    return 0;
+}
